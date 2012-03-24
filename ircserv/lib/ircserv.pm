427ebe01daa6beea6551b910fb35ccb0490111e0
Jussi Mononen <jussi.mononen@nitorcreations.com>
2012-03-24 23:15:02 +0200

    Proper template variable assignment

--- ircserv/lib/ircserv.pm
+++ ircserv/lib/ircserv.pm
@@ -1,12 +1,15 @@
 package ircserv;
 
 use Dancer ':syntax';
+use Template;
 use MongoDB;
 use MongoDB::OID;
 
 use Data::Dumper;
 
 our $VERSION = '0.1';
+
+set 'template'     => 'template_toolkit';
 
 # Initialize MongoDB connection on default port
 my $conn = MongoDB::Connection->new(host => 'localhost', port => 27017);
@@ -16,9 +19,10 @@ my $coll = $db->irc;
 
 get '/' => sub {
     my $irc_lines = $coll->find->limit(100);
-    my @irc = $irc_lines->all;
-    debug("irc ontent", \@irc);
-    template 'latest',  \@irc;
+    my %found_lines = ( 'irc' => [] );
+    push @{ $found_lines{'irc'} }, $irc_lines->all();
+    debug(%found_lines);
+    template 'latest',  \%found_lines;
 };
 
 true;
